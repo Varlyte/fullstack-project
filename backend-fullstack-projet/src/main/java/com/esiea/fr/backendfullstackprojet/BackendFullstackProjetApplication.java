15e6fdd56e5dc45e6a86ebafbf28e381e43ede48
Maxime Maitrot <maitrot@et.esiea.fr>
2022-03-14 09:24:05 +0100

    Init

---
+++ backend-fullstack-projet/src/main/java/com/esiea/fr/backendfullstackprojet/BackendFullstackProjetApplication.java
@@ -0,0 +1,13 @@
+package com.esiea.fr.backendfullstackprojet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFullstackProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFullstackProjetApplication.class, args);
+	}
+
+}
---
+++ backend-fullstack-projet/src/test/java/com/esiea/fr/backendfullstackprojet/BackendFullstackProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esiea.fr.backendfullstackprojet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFullstackProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
